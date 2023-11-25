2eb88dd66364afb31ce30fa94e83e3a86aabcb4c
Vinay Kishore <a.kishore@pw.live>
2023-11-25 14:23:53 +0530

    feat: doing the setup of client

---
+++ client/app/(routes)/page.tsx
@@ -0,0 +1,8 @@
+import '../global.scss'
+export default function Home() {
+  return (
+  <main className="">
+    <h1 className="text-[10rem] font-sans text-red-600">This is a header</h1>
+  </main>
+  )
+}
