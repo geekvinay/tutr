524f7177add0f1ec73f9f1c521f8b6209c016ff6
Vinay Kishore <a.kishore@pw.live>
2024-02-04 12:47:26 +0530

    fix: a small fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // reactStrictMode: false,
-    webpack: (config) => {
-      config.externals.push({
-        "utf-8-validate": "commonjs utf-8-validate",
-        bufferutil: "commonjs bufferutil",
-        canvas: "commonjs canvas",
-      });
-      // config.infrastructureLogging = { debug: /PackFileCache/ };
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
